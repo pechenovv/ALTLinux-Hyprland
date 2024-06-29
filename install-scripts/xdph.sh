419a0821faf9724d875f95c488e912c5528250c9
Valentin Pechenov <pe4enov.valentin@yandex.ru>
2024-06-29 14:45:15 +0300

    remove xdg checking

--- install-scripts/xdph.sh
+++ install-scripts/xdph.sh
@@ -36,35 +36,35 @@ printf "${NOTE} Checking for other XDG-Desktop-Portal-Implementations....\n"
 sleep 1
 printf "\n"
 printf "${NOTE} XDG-desktop-portal-KDE & GNOME (if installed) should be manually disabled or removed! I can't remove it... sorry...\n"
-while true; do
-  #  read -rp "${CAT} Would you like to try to remove other XDG-Desktop-Portal-Implementations? (y/n) " XDPH1
-    echo
-    sleep 1
+#while true; do
+#    read -rp "${CAT} Would you like to try to remove other XDG-Desktop-Portal-Implementations? (y/n) " XDPH1
+#    echo
+#    sleep 1
 
-    case $XDPH1 in
-        [Yy])
-            # Clean out other portals
-            printf "${NOTE} Clearing any other xdg-desktop-portal implementations...\n"
-            # Check if packages are installed and uninstall if present
-  			if sudo dnf list installed xdg-desktop-portal-wlr &>> /dev/null; then
-    		echo "Removing xdg-desktop-portal-wlr..."
-    		sudo dnf remove -y xdg-desktop-portal-wlr 2>&1 | tee -a "$LOG"
-  			fi
-
-  			if sudo dnf list installed xdg-desktop-portal-lxqt &>> /dev/null; then
-    		echo "Removing xdg-desktop-portal-lxqt..."
-    		sudo dnf remove -y xdg-desktop-portal-lxqt 2>&1 | tee -a "$LOG"
-  			fi
-
-            break
-            ;;
-        [Nn])
-            echo "no other XDG-implementations will be removed." >> "$LOG"
-            break
-            ;;
-        *)
-            echo "Invalid input. Please enter 'y' for yes or 'n' for no."
-            ;;
-    esac
-done
+#    case $XDPH1 in
+#       [Yy])
+#            # Clean out other portals
+#            printf "${NOTE} Clearing any other xdg-desktop-portal implementations...\n"
+#            # Check if packages are installed and uninstall if present
+#  			if sudo dnf list installed xdg-desktop-portal-wlr &>> /dev/null; then
+#   		echo "Removing xdg-desktop-portal-wlr..."
+#    		sudo dnf remove -y xdg-desktop-portal-wlr 2>&1 | tee -a "$LOG"
+#  			fi
+#
+#  			if sudo dnf list installed xdg-desktop-portal-lxqt &>> /dev/null; then
+#    		echo "Removing xdg-desktop-portal-lxqt..."
+#    		sudo dnf remove -y xdg-desktop-portal-lxqt 2>&1 | tee -a "$LOG"
+#  			fi
+#
+#            break
+#            ;;
+#        [Nn])
+#            echo "no other XDG-implementations will be removed." >> "$LOG"
+#            break
+#            ;;
+#        *)
+#           echo "Invalid input. Please enter 'y' for yes or 'n' for no."
+#            ;;
+#    esac
+#done
 clear
